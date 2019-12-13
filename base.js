cec9946f0a0c49f06bc407057e05dae56f599ec5
Arbust0 <Arbust908@gmail.com>
2019-12-13 10:29:15 -0300

    first commit

---
+++ base.js
@@ -0,0 +1,29 @@
+/*
+PDF 25
+Hadcover + pdf + stucker 40
+Hardcover + pdf + sticker + shirt 65
+Limited HC + pdf + sticker 80
+Limited HC + pdf + sticker + shirt 105
+Hardcover + pdf + shirt + dragon + sticker 195
+Limited HC + pdf + shirt + dragon + sticker 280
+Limited HC + pdf + shirt + dragon + sticker + cartas + screen + todo 1000
+
+Screen 20
+Cartas 20
+Limited Dragon 130
+Pharyon 65
+Tristan 15
+Sylt 45
+Rakat 45
+Zaar 40
+Haraantar 15
+Khorak 70
+vorgaut 15
+Sryz 40
+Maelodor 55
+Zoryal 45
+Indix 15
+Groyle 70
+Telcezalco 35
+Moranon 45
+*/
